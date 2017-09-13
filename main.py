69c7a96039d283127fbaa1e649c948f1d658d116
Suhas Mallya <suhasmallya97@gmail.com>
2017-09-13 22:41:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import datetime, os, shutil
 
-file_dir = "/media/batman/Kindle/documents/My Clippings.txt"
-notes_dir = "/home/batman/Documents/Kindle Notes/"
-hist_dir = "/home/batman/Documents/Kindle Notes/history/"
+# Directory of the My Clippings.txt file
+file_dir = "/media/{your username}/Kindle/documents/My Clippings.txt"
+# Directory where you want the notes to be saved to
+notes_dir = "/home/{your username}/Documents/Kindle Notes/"
+"""
+Directory to which you wish to move the My Clippings.txt file. This is done primarily
+to avoid the same notes being re-written each time you run this script
+"""
+hist_dir = "/home/{your username}/Documents/Kindle Notes/history/"
 
-"Read contents of My Clippings.txt"
 notes = open(file_dir, mode='r').read()
 
-"Each note ends with =========="
+# Kindle ends each note with '=========='
 each_note = notes.split('==========')
 
-"Dissecting each note"
+"""
+Dissecting each note into various components
+You'll probably understand this bit better if you look at the My Clippings.txt :P
+"""
 for n in range(len(each_note)-2):
     book_name = each_note[n].split('\n')[-5]
     point = each_note[n].split('\n')[-2]
@@ -21,5 +29,5 @@ for n in range(len(each_note)-2):
     f.write('\n\n')
     f.close()
 
-"Move the notes"
+"Move the notes to local drive to prevent multiple versions of the same notes"
 shutil.move(file_dir, hist_dir+'notefile'+str(datetime.datetime.now())+'.txt')
