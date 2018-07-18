adff5ae9e91202391b21681639be1a80ca0ca60e
qwer7y <suhasmallya97@gmail.com>
2018-07-18 19:21:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import datetime, os, shutil, sys
 
+
 # Directory of the My Clippings.txt file
 file_dir = "/media/{your username}/Kindle/documents/My Clippings.txt"
+
 # Directory where you want the notes to be saved to
 notes_dir = "/home/{your username}/Documents/Kindle Notes/"
+
 """
-Directory to which you wish to move the My Clippings.txt file. This is done primarily
+Directory to which you wish to move the notes from the My Clippings.txt file. This is done primarily
 to avoid the same notes being re-written each time you run this script
 """
 hist_dir = "/home/{your username}/Documents/Kindle Notes/history/"
@@ -21,7 +24,7 @@ each_note = notes.split('==========')
 
 """
 Dissecting each note into various components
-You'll probably understand this bit better if you look at the My Clippings.txt :P
+You'll probably understand this bit better if you look at the My Clippings.txt
 """
 for n in range(len(each_note)-2):
     book_name = each_note[n].split('\n')[-5]
