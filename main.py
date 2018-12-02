b13467d4635e8267d2da726a8a6453ff856980d7
qwer7y <suhasmallya97@gmail.com>
2018-12-02 10:24:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+"""
+This script reads the MyClippings.txt file from one's Kindle and sorts the notes based on the book name.
+The script also moves the MyClippings.txt file to a history directory on your computer. This is done so
+that the same notes aren't created everytime this script is run.
+"""
+
+
 import datetime, os, shutil, sys
 
 
 # Directory of the My Clippings.txt file
-file_dir = "/media/{your username}/Kindle/documents/My Clippings.txt"
+file_dir = "/media/{add_your_username_here}/Kindle/documents/My Clippings.txt"
 
 # Directory where you want the notes to be saved to
-notes_dir = "/home/{your username}/Documents/Kindle Notes/"
+notes_dir = "/home/{add_your_username_here}/Documents/Kindle Notes/"
 
 """
 Directory to which you wish to move the notes from the My Clippings.txt file. This is done primarily
 to avoid the same notes being re-written each time you run this script
 """
-hist_dir = "/home/{your username}/Documents/Kindle Notes/history/"
+hist_dir = "/home/{add_your_username_here}/Documents/Kindle Notes/history/"
 
 try:
         notes = open(file_dir, mode='r').read()
